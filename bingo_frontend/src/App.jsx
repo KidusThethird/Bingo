b0fa363f921d911f07d02c221a6b58c42fede530
kidus Tesfaye <kidus.z3rd@gmail.com>
2023-07-19 11:03:49 +0300

    vite react initialized

---
+++ bingo_frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+        Hello
+      </div>
+    </>
+  )
+}
+
+export default App

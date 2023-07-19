7e316ce4140fa711974be6b4b1e548bffe43dd87
kidus Tesfaye <kidus.z3rd@gmail.com>
2023-07-19 17:13:43 +0300

    route test made

--- bingo_frontend/src/App.jsx
+++ bingo_frontend/src/App.jsx
@@ -1,17 +1,18 @@
 import { useState } from 'react'
-
-
+import {Routes, Route, useNavigate} from 'react-router-dom';
+import Login from './components/Login';
+import Home from './container/Home';
 import './App.css'
+
 
 function App() {
  
 
   return (
-    <>
-      <h1 className='text-3xl font-bold underline'>
-        Hello
-      </h1>
-    </>
+   <Routes>
+      <Route path="login" element={<Login />}/>
+      <Route path="/*" element={<Home />}/>
+   </Routes>
   )
 }
 
